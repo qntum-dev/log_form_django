4bec3d0c9ca1c7d1344b77b624d1d4dc2a469eee
qntum-dev <pritammondal.dev@gmail.com>
2024-03-16 02:30:24 +0530

    added changes

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('register/',views.register_view,name='register'),
+    path('',views.register_view,name='register'),
     path('login/',views.login_view,name='login'),
     path('logout/',views.logout_view,name='logout'),
     path('dashboard/',views.dashboard_view,name='dashboard')
